a28216abce4669979c51257a6dc8c8c5fc96764a
peaches12 <vlaz1999@yahoo.com>
2016-10-19 19:02:22 -0400

    comitt-02

--- LED_Bar_Graph_original/LED_Bar_Graph_original.ino
+++ LED_Bar_Graph_original/LED_Bar_Graph_original.ino
@@ -1,29 +1,62 @@
 
-int ledPins[] = {4, 9, 2, 7, 5, 8, 6, 3};
-// the number of LEDs in the bar graph
-int ledCount = sizeof(ledPins) / sizeof(ledPins[0]);
+/*
+ * Tutorial 3b: LED Knight Rider
+ * 
+ * Demonstrates the use of a for() loop.
+ * Lights multiple LEDs in sequence, then in reverse.
+ *
+ * The circuit:
+ *  - 6 LEDs attached to digital pins 2 through 7 with 330ohm resistors
+ *
+ * created 2006
+ * by David A. Mellis
+ * modified 30 Aug 2011
+ * by Tom Igoe
+ * modified 14 August 2013
+ * by Blaise Jarrett
+ *
+ * This example code is in the public domain.
+ *
+ * Derivative work from:
+ * http://www.arduino.cc/en/Tutorial/ForLoop
+ *
+ */
+
+// The time in ms each LED stays on for
+// experiment with this number, the lower the number
+// the faster the LEDs change
+int timer = 100;
 
 void setup()
 {
     // use a for loop to initialize each pin as an output
-    for (int thisLed = 0; thisLed < ledCount; thisLed++)
+    for (int thisPin = 2; thisPin < 8; thisPin++)
     {
-        pinMode(ledPins[thisLed], OUTPUT);
+        pinMode(thisPin, OUTPUT);
     }
 }
 
 void loop()
 {
-    int off[], on[], on_LEDS, off_LEDS;
-    // read the potentiometer
-    int potReading = analogRead(potPin);
-    // map the result to a range from 0 to the number of LEDs
-    int ledLevel = map(potReading, 0, 1023, 0, ledCount);
-    on_LEDS = ledLevel;
-    off_LEDS = sizeof(ledPins) - on_LEDS;
-    //split ledPins into 2 separate arrays
-    memcpy(on, ledPins, on_LEDS * sizeof(int));
-    memcpy(off, &ledPins[on_LEDS], off_LEDS * sizeof(int));
+    // loop from the lowest pin to the highest
+    for (int thisPin = 2; thisPin < 8; thisPin++)
+    {
+        // turn the pin on
+        digitalWrite(thisPin, HIGH);
+        // wait to turn it off so we can see it
+        delay(timer);
+        // turn the pin off
+        digitalWrite(thisPin, LOW);
+    }
 
-    
+    // loop from the highest pin to the lowest
+    for (int thisPin = 7; thisPin > 1; thisPin--)
+    {
+        // turn the pin on
+        digitalWrite(thisPin, HIGH);
+        // wait to turn it off so we can see it
+        delay(timer);
+        // turn the pin off
+        digitalWrite(thisPin, LOW);
+    }
 }
