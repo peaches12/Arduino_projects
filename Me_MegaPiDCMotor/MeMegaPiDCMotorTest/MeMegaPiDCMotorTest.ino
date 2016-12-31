0bc1ae3c0467032b4d9fc4c0d44dd248f1bb1b54
peaches12 <vlaz1999@yahoo.com>
2016-12-31 16:17:07 -0500

    version-2

--- Me_MegaPiDCMotor/MeMegaPiDCMotorTest/MeMegaPiDCMotorTest.ino
+++ Me_MegaPiDCMotor/MeMegaPiDCMotorTest/MeMegaPiDCMotorTest.ino
@@ -18,27 +18,33 @@ void loop()
 {
   motor2.run(-255);
   motor4.run(255);
-  delay(3190); //table to hall
-  motor4.stop();
+  delay(5000); //table to hall 
+  motor4.run(255);
   motor2.run(255);
-  delay(2000); // turn right
+  delay(900); // turn right
   motor2.run(-255);
   motor4.run(255);
   delay(10000); //hall to bedroom
-  motor4.stop();
   motor2.run(255);
-  delay(2000); // turn right
+  motor4.run(255);
+  delay(200);
+  motor2.run(-255);
+  motor4.run(255);
+  delay(10000); //hall to bedroom
+  motor4.run(255);
+  motor2.run(255);
+  delay(800); // turn right
   motor2.run(-255);
   motor4.run(255);
   delay(3190); //into the bedroom
-  motor2.stop();
-  motor4.run(255);
-  delay(2000);
+  motor2.run(-255);
+  motor4.run(-255);
+  delay(800); //turn left
   motor2.run(-255);
   motor4.run(255);
   delay(3000);
   motor4.stop();
-  motor2.stop();
+  motor2.stop(); 
   
 }
 
